300c561a86ff2e76967b72f262319c46dbfaa0eb
Do Quyen <doquyen2404@gmail.com>
2022-01-10 13:23:28 +0700

    no message

--- untitled/src/test/java/Day1/Car.java
+++ untitled/src/test/java/Day1/Car.java
@@ -6,4 +6,51 @@ public class Car {
     String door;
     String engine;
     String seat;
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public String getWheel() {
+        return wheel;
+    }
+
+    public void setWheel(String wheel) {
+
+        this.wheel = wheel;
+    }
+
+    public String getDoor() {
+
+        return door;
+    }
+
+    public void setDoor(String door) {
+
+        this.door = door;
+    }
+
+    public String getEngine() {
+
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+
+        this.engine = engine;
+    }
+
+    public String getSeat() {
+
+        return seat;
+    }
+
+    public void setSeat(String seat) {
+
+        this.seat = seat;
+    }
 }
--- untitled/src/test/java/Day1/HelloWorld.java
+++ untitled/src/test/java/Day1/HelloWorld.java
@@ -1,7 +1,17 @@
 package Day1;
 
+import javax.sound.midi.Soundbank;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String txt = "Hello World";
+
+        System.out.println(txt);
+        System.out.println(txt.toUpperCase());
+        System.out.println(txt.toUpperCase());
+        System.out.println(txt.length());
+        System.out.println(txt.substring(5));
+        System.out.println(txt.substring(2,5));
+        System.out.println(txt.substring(txt.length()-2, txt.length()));
     }
 }
